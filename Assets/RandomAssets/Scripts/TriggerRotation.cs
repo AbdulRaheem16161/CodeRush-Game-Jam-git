cadf0e7818b9b8e846adb6af7934a34f610d2cce
AbdulRahman Usman <abdulraheem16161@gmail.com>
2026-05-07 14:35:06 +0500

    made some progress in gravity shifting

--- Assets/RandomAssets/Scripts/TriggerRotation.cs
+++ Assets/RandomAssets/Scripts/TriggerRotation.cs
@@ -3,17 +3,17 @@ using UnityEngine;
 public class TriggerRotation : MonoBehaviour
 {
     public SmoothRotateToTarget smoothRotateToTarget;
-    public Vector3 targetRotation = new Vector3(0f, 0f, 0f);  
+    public Vector3 targetRotation = new Vector3(0f, 0f, 0f);
 
     public string name;
 
-    private void OntriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger entered by: " + other.name + " on trigger: " + name);
-        
+
         if (other.CompareTag("Player"))
         {
-           smoothRotateToTarget.targetRotation = targetRotation;  
+            smoothRotateToTarget.targetRotation = targetRotation;
         }
     }
 }
